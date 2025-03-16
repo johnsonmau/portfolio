40061fd4e522c4942f6bc4325fc3597fc96f9f4c
Maurice Johnson <maurice.johnson9911@gmail.com>
2025-03-16 00:50:55 -0400

    adding 404 page + refactoring

--- src/main/java/com/johnson/portfolio/Controllers/MainController.java
+++ src/main/java/com/johnson/portfolio/Controllers/MainController.java
@@ -1,13 +1,26 @@
 package com.johnson.portfolio.Controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/home")
-    public String getIndex(){
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    @GetMapping("/")
+    public String getIndex(HttpServletRequest request){
+
+        String clientIp = request.getHeader("X-Forwarded-For");
+
+        if (clientIp == null){
+            clientIp = request.getRemoteAddr();
+        }
+        logger.info("user [{}] entered homepage",clientIp);
+
         return "index";
     }
 
